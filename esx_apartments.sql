3fc179bd7d487f3239b8cd1f9f9723f5b90de785
forward2k <forward2k@tuta.io>
2019-11-22 19:35:18 +0100

    add houses

--- esx_apartments.sql
+++ esx_apartments.sql
@@ -42,7 +42,16 @@ INSERT INTO `apartments_available` VALUES
 	(NULL, 'RichardMajestic', 'Richard Majestic, Apt 2', 0, '-937.06,-379.45,38.96,106.63', '-913.24,-365.58,114.27,114.09', NULL, NULL, NULL, NULL, 2000, 1000000, 50),
 	(NULL, 'Tinsle Towers', 'Tinsle Towers, Apt 42', 0, '-614.54,45.53,43.59,179.3', '-603.76,58.89,98.2,86.34', NULL, NULL, NULL, NULL, 2000, 1000000, 50),
 	(NULL, 'EclipseTowers', 'EclipseTowers, Apt 3', 0, '-777.00,318.5,85.66,175.99', '-784.56,323.8,212.0,265.24', NULL, NULL, NULL, NULL, 2500, 1200000, 50),
+;
+
+-- Houses
+INSERT INTO `apartments_available` VALUES
 	(NULL, '3655 Wild Oats Drive', '3655 Wild Oats Drive', 1, '-175.71,502.49,137.42,73.94', '-174.15,497.03,137.67,188.06', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
 	(NULL, '2044 North Conker Avenue', '2044 North Conker Avenue', 1, '346.97,441.08,147.70,293.58', '341.40,437.45,149.39,114.50', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
 	(NULL, '2045 North Conker Avenue', '2045 North Conker Avenue', 1, '373.01,428.08,145.68,72.91', '373.60,422.88,145.91,164.92', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
+	(NULL, '2862 Hillcrest Avenue', '2862 Hillcrest Avenue', 1, '-686.66,596.68,143.64,41.40', '-681.84,591.85,145.39,224.93', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
+	(NULL, '2868 Hillcrest Avenue', '2868 Hillcrest Avenue', 1, '-752.06,620.68,142.21,293.15', '-759.22,618.57,144.15,113.06', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
+	(NULL, '2874 Hillcrest Avenue', '2874 Hillcrest Avenue', 1, '-853.34,696.30,148.78,14.22', '-859.95,690.74,152.86,186.81', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
+	(NULL, '2677 Whispymound Drive', '2677 Whispymound Drive', 1, '119.30,564.55,183.96,6.17', '117.17,559.40,184.3,185.31', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
+	(NULL, '2133 Mad Wayne Thunder', '2133 Mad Wayne Thunder', 1, '-1294.49,455.13,97.42,11.75', '-1289.76,449.02,97.9,186.63', NULL, NULL, NULL, NULL, 3000, 1500000, 50),
 ;
